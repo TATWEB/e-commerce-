1f6437179aef5f78f6b6eda7624a37493fc5f028
Telman <telman.weber39@gmail.com>
2022-05-24 10:47:58 +0400

    happy new year message

--- src/test/java/stepDefinitions/DBsprint5Defs.java
+++ src/test/java/stepDefinitions/DBsprint5Defs.java
@@ -3,7 +3,9 @@ package stepDefinitions;
 public class DBsprint5Defs {
   // removed the password and added to config
 
-  // another line to see the conflict 
+  // another line to see the conflict
+
+    // happy new Year
 }
 
  
